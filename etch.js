36f06e2b93a3e883f2eb24c7a5d4f4df0c476667
Nick White <nawhi3@outlook.com>
2017-05-25 15:59:32 +0100

    Create grid divs dynamically with jQuery

--- etch.js
+++ etch.js
@@ -1,3 +1,20 @@
-$(document).ready(function() {
-	$('#container > div').
-})
+$(document).ready(makeGrid);
+
+var GRIDSIZE = 16;
+
+function makeGrid() {
+	console.log("Making grid");
+	for(var i=0; i<GRIDSIZE*GRIDSIZE; i++)
+	{
+		// $newDiv.addTo($('#container'));
+		var $newDiv = $("<div></div>");
+		$newDiv.addClass("gridsquare");
+		$newDiv.attr('id', 'row' + parseInt(i/GRIDSIZE));
+		var w = parseInt($('#container').width()/GRIDSIZE) + 'px';
+		console.log("setting width to " + w);
+		$newDiv.width(w);
+		$newDiv.height(w);
+		$newDiv.attr('border-radius', parseInt(w/5));
+		$('#container').append($newDiv);
+	}
+}
